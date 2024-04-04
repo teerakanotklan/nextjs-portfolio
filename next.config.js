3d6479d8283fffd229c0960ed4526c372bbb560e
Teerakan <teerakan.otk@gmail.com>
2024-04-05 01:20:43 +0700

    Update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-  // output: "export",
-  // distDir: "dist",
-  // images: { unoptimized: true },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
